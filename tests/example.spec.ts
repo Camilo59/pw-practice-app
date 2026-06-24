9374d1e78fb6fd3e29df29f496ca34b80f50736b
Camilo59 <95262546+Camilo59@users.noreply.github.com>
2026-06-24 15:50:19 -0500

    42.DatePicker8:20min

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,5 +8,19 @@ test.beforeEach(async ({page}) => {
 test('datepicker', async ({page}) => {
   await page.getByText('Forms').click()
   await page.getByText('Datepicker').click()
-  
+
+  const calendarInputField = page.getByPlaceholder('Form Picker')
+  await calendarInputField.click()
+
+  let date = new Date()
+  date.setDate(date.getDate() + 1)
+  const expectedDate = date.getDate().toString()
+  const expectedMonthShot = date.toLocaleString('En-US', {month: 'short'})
+  const expectedYear = date.getFullYear()
+  const dateToAssert = `${expectedMonthShot}`
+
+
+  await page.locator('[class="day-cell ng-star-inserted"]').getByText('1', {exact: true}).click()
+  await expect(calendarInputField).toHaveValue('Jun 21, 2023')
+
 })
