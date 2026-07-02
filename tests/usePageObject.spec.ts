c2373f3228514fd191342bf306258a667d3a1e28
Camilo59 <95262546+Camilo59@users.noreply.github.com>
2026-07-01 22:21:18 -0500

    50.DatePickerPageObject

--- tests/usePageObject.spec.ts
+++ tests/usePageObject.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test'
 import {NavigationPage} from '../page-objects/navigationPage'
+import { formLayoutsPage } from '../page-objects/formLayoutsPage'
 
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
@@ -12,4 +13,13 @@ test('navigate to form page', async({page}) => {
     await navigateTo.smartTablePage()
     await navigateTo.toastrPage()
     await navigateTo.tooltipPage()
+})
+
+test('parametrized methods', async({page}) => {
+    const navigateTo = new NavigationPage(page)
+    const onFormLayoutsPage = new formLayoutsPage(page)
+
+    await navigateTo.formLayoutsPage()
+    await onFormLayoutsPage.submitUsingTheGrigdFormWithCredentialsAndSelectOption('tesdt@test.com','welcome1','Option 2')
+    await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', true)
 })
